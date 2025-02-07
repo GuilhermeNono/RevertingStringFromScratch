dff7340febf5b7aa2f88ba4fd8e9843bad591fae
Guilherme Nono <guilherme.nono45789@gmail.com>
2025-02-06 22:19:15 -0300

    feat: Reverting a string from scratch

---
+++ LowAbstraction/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LowAbstraction;
+
+unsafe class Program
+{
+    static void Main(string[] args)
+    {
+        string hello = "Hello World!";
+        int stringQuantity = hello.Length;
+        char[] helloChars = new char[stringQuantity]; 
+
+        fixed (char* cPtr = hello)
+        {
+            for (int i = 0; i < stringQuantity; i++)
+            {
+                helloChars[i] = cPtr[stringQuantity - i - 1];
+            }
+        }
+
+        Console.WriteLine(helloChars);
+    }
+}
